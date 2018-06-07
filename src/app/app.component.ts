056b06bc28a25bcbc7d8d065ff7e903e880f5046
joaopavila <joaopaulo_avila@hotmail.com>
2018-06-07 01:08:18 -0300

    Update typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
 
   public properties = {
     photoInterval: 3000,
